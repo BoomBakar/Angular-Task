e425794bce4b3a31c0eca6b78ffdc3e891fa7c4b
BoomBakar <abubakardar608@gmail.com>
2023-08-31 11:18:21 +0500

    loading animation added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MoviesComponent } from './components/movies/movies.component';
 import { DirectorsComponent } from './components/directors/directors.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ErrComponent } from './components/err/err.component';
 import { AccessDeniedComponent } from './components/access-denied/access-denied.component';
 import { UiSwitchModule } from 'ngx-ui-switch';
+import { SpinnerComponent } from './components/spinner/spinner.component';
+import { LoadingInterceptor } from './loading.interceptor';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,8 @@ import { UiSwitchModule } from 'ngx-ui-switch';
     MoviesComponent,
     DirectorsComponent,
     ErrComponent,
-    AccessDeniedComponent
+    AccessDeniedComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
@@ -33,7 +36,9 @@ import { UiSwitchModule } from 'ngx-ui-switch';
     ReactiveFormsModule,
     UiSwitchModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true
+}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
